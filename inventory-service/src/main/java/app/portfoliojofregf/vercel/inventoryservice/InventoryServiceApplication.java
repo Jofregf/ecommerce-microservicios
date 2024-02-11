40070fac4226dc9e3164b5b2a08c6ba64b78442d
Guillermo Jofre <jofregf@gmail.com>
2024-02-10 23:38:10 -0300

    Discovery Server

--- inventory-service/src/main/java/app/portfoliojofregf/vercel/inventoryservice/InventoryServiceApplication.java
+++ inventory-service/src/main/java/app/portfoliojofregf/vercel/inventoryservice/InventoryServiceApplication.java
@@ -5,9 +5,11 @@ import app.portfoliojofregf.vercel.inventoryservice.repository.InventoryReposito
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class InventoryServiceApplication {
 
     public static void main(String[] args) {
